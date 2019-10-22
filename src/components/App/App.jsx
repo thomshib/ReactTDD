753d8f27ed1e8759fa16a8f375e084445a508a9d
Shibu Thomas <shibu_st@yahoo.com>
2019-10-22 09:25:14 +0530

    Added Calculator

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import Calculator from '../Calculator/Calculator';
 
-const App = () =>(
-     <div className="app-container">
-         <Calculator />
-         </div>
-    );
-
+const App = () => (
+    <div className="app-container">
+      <Calculator />
+    </div>
+  );
+  
 export default App;
-
