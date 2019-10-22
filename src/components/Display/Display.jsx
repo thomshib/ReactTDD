3be340d7d2cc65b4477cca4b3b22bc67da975998
Shibu Thomas <shibu_st@yahoo.com>
2019-10-22 11:45:00 +0530

    added display

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes  from 'prop-types';
+import './Display.css';
 
-const Display = ( {displayValue} ) => <div className="display-container"></div>;
+const Display = ( {displayValue} ) => (
+<div className="display-container">
+    <p className="display-value">
+        {displayValue}
+    </p>
+    </div>);
 
 Display.propTypes = { displayValue: PropTypes.string.isRequired};
 
